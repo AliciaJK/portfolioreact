2981ecba3086ae510fc1221db7364f1e718af76b
AliciaJK <alicia.krolak4@gmail.com>
2021-07-23 23:23:31 -0400

    formatting

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,16 +3,13 @@ import React from 'react';
 export default function About() {
   return (
     <div>
- <p>
-      <ul>
-                <li><a href="https://docs.google.com/document/d/1bv0sZlwjcShO83H3GZTN6gry_xXh7EE3nlp2iOCtWA0/edit?usp=sharing">My Resume</a></li>
-
-            </ul>
-
-
+      <p>
+        <ul>
+          <li><a href="https://docs.google.com/document/d/1bv0sZlwjcShO83H3GZTN6gry_xXh7EE3nlp2iOCtWA0/edit?usp=sharing">My Resume</a>
+          </li>
+        </ul>
       </p>
-     
     </div>
-     
+
   );
 }
