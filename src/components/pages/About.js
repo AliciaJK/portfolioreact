d8ed46c4f1c24d7ffa97a49abcf62d44d03595e7
AliciaJK <alicia.krolak4@gmail.com>
2021-07-23 23:41:52 -0400

    changed contact cards

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div>
+    <div className="Resume">
       <p>
         <ul>
           <li><a href="https://docs.google.com/document/d/1bv0sZlwjcShO83H3GZTN6gry_xXh7EE3nlp2iOCtWA0/edit?usp=sharing">My Resume</a>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,19 +1,36 @@
 import React from 'react';
+import Card from 'react-bootstrap/card'
+import CardGroup from 'react-bootstrap/cardgroup'
 
 export default function Contact() {
   return (
     <div>
       <h1>Contact Me</h1>
-      <p>
-      <ul>
-                <li><a href="mailto:aliciajkrolak@gmail.com">By email at alicia.krolak4@gmail.com </a></li>
-                <li><a href="https://www.linkedin.com/in/aliciakrolak/">On LinkedIn at Alicia Krolak</a></li>
-                <li><a href="http://www.github.com/aliciajk">On Github via AliciaJK</a></li>
-            </ul>
+ <CardGroup>
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://1000logos.net/wp-content/uploads/2017/03/Linkedin-Logo.png" />
+  <Card.Body>
+    <Card.Title>My LinkedIn</Card.Title>
+    <Card.Link href="https://www.linkedin.com/in/aliciakrolak/">Click to view</Card.Link>
+  </Card.Body>
+</Card>
 
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://image.flaticon.com/icons/png/512/25/25231.png" />
+  <Card.Body>
+    <Card.Title>My GitHub</Card.Title>
+    <Card.Link href="http://www.github.com/aliciajk">Click to view</Card.Link>
+  </Card.Body>
+</Card>
 
-      </p>
-
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://www.pngitem.com/pimgs/m/386-3860252_welcome-email-icon-transparent-hd-png-download.png" />
+  <Card.Body>
+    <Card.Title>Email Alicia</Card.Title>
+    <Card.Link href="mailto:aliciajkrolak@gmail.com">Click to email</Card.Link>
+  </Card.Body>
+</Card>
+</CardGroup>
     </div>
   );
 }
