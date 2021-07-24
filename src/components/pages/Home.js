e5198683daf33bfc0069427617b99e9b6d0ce964
AliciaJK <alicia.krolak4@gmail.com>
2021-07-24 10:37:58 -0400

    update styling

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>
+      <h1 style={{margin: '25px'}}>Home Page</h1>
+      <p style={{margin: '50px'}} >
       Welcome to my portfolio! I am a Cognitive Science graduate from Carleton University in 2019 with a specialization in cognition and psychology and a minor in psychology.
 
 I have a background in aviation with my glider and private pilot license. I recently started on a new endeavour: Swift Drone Academy Inc. We are a small business in the Ottawa area focused on providing focused and fully inclusive Drone training from start to finish. Check us out at SwiftDroneAcademy.ca.
